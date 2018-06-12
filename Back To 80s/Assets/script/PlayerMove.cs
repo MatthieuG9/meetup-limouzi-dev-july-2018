52740b2666432267e3fcae3755b7c9b232a070b9
Matthieu Giraud <matthieu.giraud99@gmail.com>
2018-06-12 22:27:22 +0200

    Mise en place des colliders

---
+++ Back To 80s/Assets/script/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    [SerializeField]
+    private float moveForce = 600f;
+    [SerializeField]
+    private float maxSpeed = 10f;
+    [SerializeField]
+    private float jumpForce = 2000f;
+
+    private bool facingRight = true;
+    private bool jump = false;
+    private bool grounded = false;
+
+    private Transform top;
+    private Transform bottom;
+    private int groundMask;
+
+    void Start () {
+        top = transform.Find("top");
+        bottom = transform.Find("bottom");
+        groundMask = 1 << LayerMask.NameToLayer("Ground");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        grounded = Physics2D.Linecast(top.position, bottom.position, groundMask);
+        Debug.Log(grounded);
+	}
+}
--- readme.txt
+++ readme.txt
@@ -5,3 +5,12 @@ Des sprites
 Des sorting layers
 Important de bien organiser (gameobjects parent + position à zero)
 Création d'une skybox (présentation rapide des materials).
+
+
+2- Mise en place des colliders et de la gravité
+
+Explication sur les box2d
+Mise en place d'un top et d'un bottom (qui doit dépasser du collider)
+Attention au layer
+Création du material physics
+Création du script grounded
