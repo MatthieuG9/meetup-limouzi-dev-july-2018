27e848c7eb524c8ffef6310c4c6839b805608a81
Matthieu Giraud <matthieu.giraud99@gmail.com>
2018-06-18 19:52:28 +0200

    demo step 1

--- Back To 80s/Assets/script/Weapon.cs
+++ Back To 80s/Assets/script/Weapon.cs
@@ -4,21 +4,5 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour {
 
-    private GameObject parent;
 
-	void Start () {
-        parent = transform.parent.gameObject;	
-	}
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject != parent)
-        {
-            var player = collision.gameObject.GetComponent<PlayerMove>();
-            if(player)
-            {
-                player.hit();
-            }
-        }
-    }
 }
